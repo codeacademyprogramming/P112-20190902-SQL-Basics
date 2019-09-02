43a08d0693a0d97437f0e8e9efda11e8c1ab49f2
Sattar Amrahov <sattar@code.local>
2019-09-02 13:03:43 +0400

    links added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # P112-20190902-SQL-Basics
+
+Faydalı linklər:
+
+- https://www.w3schools.com/sql/default.asp
+- https://www.w3schools.com/sql/sql_delete.asp
+
+- https://www.w3schools.com/sql/sql_create_db.asp
+- https://www.w3schools.com/sql/sql_alter.asp
---
+++ Solution20190902/TaskWarehouse/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskWarehouse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            GetNumber();
+            int a, b, c;
+            Test(out a, out b, out c);
+
+
+            string num = "asd";
+            int r;
+            int.TryParse(num, out r);
+
+            MessageBox.Show("a = {0}, b = {1}, c = {2}", a.ToString(), b.ToString(), c.ToString());
+            MessageBox.Show($"{r}");
+
+        }
+
+        public int GetNumber()
+        {
+            return 55;
+        }
+
+        public void Test(out int birinci, out int ikinci, out int ucuncu)
+        {
+            birinci = 10;
+            ikinci = 20;
+            ucuncu = 30;
+        }
+    }
+}
